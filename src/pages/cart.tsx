8f9f4dbc3061d580846223d77c1f2095aba6aa3d
Daniel Santos <danielsc.let@gmail.com>
2022-05-09 19:28:49 -0300

    feat: create initial structure

---
+++ src/pages/cart.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from "next";
+
+const CartPage: NextPage = () => <h1>Cart</h1>;
+
+export default CartPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from "next";
+
+const IndexPage: NextPage = () => <h1>Hello world!</h1>;
+
+export default IndexPage;
