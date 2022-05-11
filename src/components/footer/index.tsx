e44aca10904e05c118c112c29def8a20d2577e58
Daniel Santos <danielsc.let@gmail.com>
2022-05-11 17:47:46 -0300

    fix: add padding in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>beba com moderação</p>
+      <p>Beba com moderação</p>
     </footer>
   );
 };
