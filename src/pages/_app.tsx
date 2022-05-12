efdfee35f3ee71c72ae856ddb1ff01feb53d57d0
Daniel Santos <danielsc.let@gmail.com>
2022-05-12 18:41:56 -0300

    feat: configure persist gate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import { store } from "@store/index";
 
 import "@assets/styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp = ({ Component, pageProps }) => {
+  const persistor = persistStore(store);
+
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+
+      <ToastContainer position="bottom-center" hideProgressBar />
     </Provider>
   );
 };
