95382e31001cf1b695f6f59513d1ee07e626a6db
Daniel Santos <danielsc.let@gmail.com>
2022-05-12 16:17:46 -0300

    feat: create first page structure

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,20 @@
 import type { NextPage } from "next";
 
-const CartPage: NextPage = () => <h1>Cart</h1>;
+import { Footer } from "@components/footer";
+import { Header } from "@components/header";
+
+const CartPage: NextPage = () => {
+  return (
+    <main>
+      <Header />
+
+      <section>
+        <h1>Cart</h1>
+      </section>
+
+      <Footer />
+    </main>
+  );
+};
 
 export default CartPage;
